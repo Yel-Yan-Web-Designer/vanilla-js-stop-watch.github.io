9ec78e7a3fc4c0443634dad700020c5088239457
[Yel Yan] <[kmin2957@gmail.com]>
2022-12-06 20:05:30 +0300

    save in local but error in start and continue btn after loading page

--- main.js
+++ main.js
@@ -4,13 +4,9 @@ let stopWatch = document.querySelector(".stop-watch"),
     restartBtn = document.querySelector(".restart"),
     continueBtn = document.querySelector(".continue");
 
-/**
- * Create four variables that keep track of hours,mins, and seconds
- * Set all of them to zero at the beginning ?
- * Data type wll be "number"
- */
-let [hours, mins, seconds, milliseconds] = [0, 0, 0, 0];
 
+let [hours, mins, seconds, milliseconds] = [0, 0, 0, 0];
+let displayWatch;
 
 // reusable funtion to run watch 
 function startTime () {
@@ -41,9 +37,11 @@ function startTime () {
     let s = seconds < 10 ? "0" + seconds : seconds;
     let ms = milliseconds < 100 ? "0" + milliseconds : milliseconds;
 
-    return  stopWatch.textContent =  `${h} : ${m} : ${s} : ${ms}`;
-}
+    displayWatch = `${h} : ${m} : ${s} : ${ms}`;
 
+    localStorage.setItem("saveWatch", displayWatch);
+    stopWatch.textContent =  displayWatch || localStorage.getItem("saveWatch");
+}
 // keep track of setInterval set in global
 let intervalId;
 // start the watch 
@@ -59,9 +57,30 @@ pauseBtn.addEventListener("click", () => {
 restartBtn.addEventListener("click", ()=>{
     [hours, mins, seconds, milliseconds] = [0, 0, 0, 0];
     stopWatch.textContent =  `${hours}0 : ${mins}0 : ${seconds}0 : ${milliseconds}00`;
+    localStorage.clear(displayWatch);
 })
 continueBtn.addEventListener("click", ()=> {
     //clearInterval first if not starttime will run double
-    clearInterval(intervalId)
+    clearInterval(intervalId);
     intervalId = setInterval(startTime, 10);
 })
+
+
+window.addEventListener("load", () => {
+    const saveWatch = localStorage.getItem("saveWatch");
+
+    if( saveWatch){
+        stopWatch.textContent = saveWatch;
+    }
+})
+
+/**
+ * Create four variables that keep track of hours,mins, and seconds, milliseconds
+ * Set all of them to zero at the beginning ?
+ * Data type wll be "number"
+ */
+
+/**
+ * Problem, save in local storage when we click start btn, continue btn
+ * clear local storage when click restart
+ */
